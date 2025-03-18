3ab61e2b7f5e6e13130bab5bc30d04406a8daa6c
NurZukhrufiyatiSartikaPutri <ssartikaaputrii@gmail.com>
2025-03-18 20:15:58 +0700

    first commit

---
+++ file1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Pecahan naik
+    int i;
+    for (i = 0; i < 5; i++){
+        cout << "Nilai i = "<< i <<" Lari Keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+
+    //Pecahan Turun
+    for (i = 5; i > 0; i--){
+        cout << "Nilai i = "<< i <<" Lari Keliling Lapangan" << endl;
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+}
